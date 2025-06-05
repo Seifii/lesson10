e9f8869d12a1a10236d63774dabce727852abe50
Seifii <sofiapetrova893@gmail.com>
2025-06-05 09:51:33 +0300

    2

--- main.py
+++ main.py
@@ -61,51 +61,112 @@ while n != 0:
     cv2.imshow("Cat 1", image1)
     print("Авввв! Він перший і ************")
     n -= 1
-    print("")
+    rates[0] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 2", image2)
     print("Другий і ************")
     n -= 1
+    rates[1] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 3", image3)
-    print("Другий і ************")
+    print("Третій і ************")
     n -= 1
+    rates[2] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 4", image4)
-    print("Другий і ************")
+    print("Четвертий і ************")
     n -= 1
+    rates[3] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 5", image5)
-    print("Другий і ************")
+    print("П'ятий і ************")
     n -= 1
+    rates[4] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 6", image6)
-    print("Другий і ************")
+    print("Шостий і ************")
     n -= 1
+    rates[5] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 7", image7)
-    print("Другий і ************")
+    print("Сьомий і ************")
     n -= 1
+    rates[6] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 8", image8)
-    print("Другий і ************")
+    print("Восьмий і ************")
     n -= 1
+    rates[7] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 9", image9)
-    print("Другий і ************")
+    print("Дев'ятий і ************")
     n -= 1
+    rates[8] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 10", image10)
-    print("Другий і ************")
+    print("Десятий і ************")
     n -= 1
+    rates[9] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 11", image11)
-    print("Другий і ************")
+    print("Одинадцятий і ************")
     n -= 1
+    rates[10] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 12", image12)
-    print("Другий і ************")
+    print("Дванадцятий і ************")
     n -= 1
+    rates[11] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 13", image13)
-    print("Другий і ************")
+    print("Тринадцятий і ************")
     n -= 1
+    rates[12] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 14", image14)
-    print("Другий і ************")
+    print("Чотирнадцятий і ************")
     n -= 1
+    rates[13] = int(input("Оцініть котика від 1 до 10: "))
     cv2.imshow("Cat 15", image15)
-    print("Другий і ************")
+    print("Останній та п'ятнадцятий ************")
     n -= 1
+    rates[15] = int(input("Оцініть котика від 1 до 10: "))
 
-cv2.imshow("Hihiii", imagew)
+if max(rates) == rates[0]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 1", image1)
+if max(rates) == rates[1]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 2", image2)
+if max(rates) == rates[2]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 3", image3)
+if max(rates) == rates[3]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 4", image4)
+if max(rates) == rates[4]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 5", image5)
+if max(rates) == rates[5]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 6", image6)
+if max(rates) == rates[6]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 7", image7)
+if max(rates) == rates[7]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 8", image8)
+if max(rates) == rates[8]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 9", image9)
+if max(rates) == rates[9]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 10", image10)
+if max(rates) == rates[10]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 11", image11)
+if max(rates) == rates[11]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 12", image12)
+if max(rates) == rates[12]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 13", image13)
+if max(rates) == rates[13]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 14", image14)
+if max(rates) == rates[14]:
+    print("Огооо! Найбільше вам сподобався ********")
+    cv2.imshow("Cat 15", image15)
+
+
+
 cv2.waitKey()
 
 
